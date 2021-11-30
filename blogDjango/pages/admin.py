bab62cd2665102056012307679decb3db216e018
JBolo <jaubmas@gmail.com>
2021-11-30 13:18:49 -0800

    Categories and panel admin features added

--- blogDjango/pages/admin.py
+++ blogDjango/pages/admin.py
@@ -1,8 +1,15 @@
+from re import search
 from django.contrib import admin
 from .models import Page
 # Register your models here.
 
-admin.site.register(Page)
+#adding more configurations
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('hidden',)
+    list_display = ('title', 'hidden', 'created_at')
+admin.site.register(Page, PageAdmin)
 
 title = "Panel de gestión"
 subtitle = "Administración"
